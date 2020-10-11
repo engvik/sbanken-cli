169c8076b6e6e3b487c156eceb444c483edc0d09
Lars Jansøn Engvik <lars@engvik.nu>
2020-10-11 14:25:45 +0200

    feature(cli): add aliases

--- internal/cli/flags.go
+++ internal/cli/flags.go
@@ -10,21 +10,25 @@ func getGlobalFlags() []cli.Flag {
 		altsrc.NewStringFlag(&cli.StringFlag{
 			Name:    "client-id",
 			Usage:   "the client id",
+			Aliases: []string{"clid"},
 			EnvVars: []string{"SBANKEN_CLIENT_ID"},
 		}),
 		altsrc.NewStringFlag(&cli.StringFlag{
 			Name:    "client-secret",
 			Usage:   "the client secret",
+			Aliases: []string{"s"},
 			EnvVars: []string{"SBANKEN_CLIENT_SECRET"},
 		}),
 		altsrc.NewStringFlag(&cli.StringFlag{
 			Name:    "customer-id",
 			Usage:   "customer id",
+			Aliases: []string{"cuid"},
 			EnvVars: []string{"SBANKEN_CUSTOMER_ID"},
 		}),
 		&cli.StringFlag{
-			Name:  "config",
-			Usage: "path to YAML config",
+			Name:    "config",
+			Usage:   "path to YAML config",
+			Aliases: []string{"c"},
 		},
 	}
 }
